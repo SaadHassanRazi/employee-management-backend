7895454e4f938f9a4def530308a1a177513bfb99
SaadHassanRazi <saadhassanrazi@gmail.com>
2025-03-21 18:07:14 +0500

    dot env commit

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { employeeRouter } from "./employee.routes";
 dotenv.config();
 
 const { ATLAS_URL } = process.env;
+const baseURL = process.env.BASE_URL || "http://localhost"; // Default to localhost if not specified in .env
+const port = process.env.PORT || 3000; // Default to 3000 if not specified in .env
 
 if (!ATLAS_URL) {
   console.error("Atlas URL is not present in dotenv");
@@ -17,10 +19,11 @@ connectToDataBase(ATLAS_URL)
   .then(() => {
     const app = express();
     app.use(cors());
-    app.use(express.json())
-    app.use('/employees',employeeRouter)
-    app.listen(3000, () => {
-      console.log(`Server is running on port 3000`);
+    app.use(express.json());
+    app.use('/employees', employeeRouter);
+    
+    app.listen(port, () => {
+      console.log(`Server is running on ${baseURL}:${port}`);
     });
   })
   .catch((error) => console.error(error));
